6371ff26411ee3c71d6b0aabc4d062bf52b6a6d3
Alejandro F <afuste7@gmail.com>
2020-09-23 11:28:06 -0400

    added console logs to functional calls

--- arrays/divisors.js
+++ arrays/divisors.js
@@ -1,9 +1,10 @@
 function divisors(integer) {
 	var res = [];
 	for (var i = 2; i <= Math.floor(integer / 2); ++i) if (integer % i == 0) res.push(i);
-	return res.length ? res : integer + ' is prime';
+	return res.length === 0 ? `${integer} is prime` : res;
 }
 
-divisors(15);
-divisors(12);
-divisors(13);
+console.log(divisors(15));
+console.log(divisors(12));
+console.log(divisors(1400));
+console.log(divisors(13));
