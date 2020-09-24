966f1b51a8136bbf28ce2fd5b3117ba646160dbd
Alejandro F <afuste7@gmail.com>
2020-09-24 17:39:26 -0400

    added binary exercise code to addBinary.js file in strings folder

---
+++ strings/addBinary.js
@@ -0,0 +1,28 @@
+/*
+
+Exercise Goal: Create a function that will add to numbers together and 
+then convert those numbers to binary. 
+
+*/
+
+// My Solution
+
+// function addBinary(a, b) {
+// 	// Add a and b and store value in sum variable
+// 	let sum = a + b;
+
+// 	// Convert sum to binary with toString method with 2 as the radix
+// 	return sum.toString(2);
+// }
+
+// console.log(addBinary(5, 5));
+// console.log(addBinary(237, 656));
+
+// Other Solution
+
+function addBinary(a, b) {
+	return (a + b).toString(2);
+}
+
+console.log(addBinary(5, 5));
+console.log(addBinary(237, 656));
