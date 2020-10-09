97f215d13c59ad0e49868fc19586bf32f06fb25f
Alejandro F <afuste7@gmail.com>
2020-10-09 17:28:54 -0400

    added two files to folder...stringToCamelCase.js in strings folder and validParentheses.js in validation folder

---
+++ validation/validParentheses.js
@@ -0,0 +1,33 @@
+/*
+
+
+Instructions:
+    Write a function called that takes a string of parentheses, and determines if the order of 
+    the parentheses is valid. The function should return true if the string is valid, and false 
+    if it's invalid.
+
+Examples:
+    "()"              =>  true
+    ")(()))"          =>  false
+    "("               =>  false
+    "(())((()())())"  =>  true
+
+Sample Tests:
+    Test.assertEquals(validParentheses( "()" ), true);
+    Test.assertEquals(validParentheses( "())" ), false);
+
+    const true1 = '()';
+    const true2 = '()()';
+    const true3 = '(())';
+
+    const false1 = '())':
+    const false2 = ")(()))";
+    const false3 = "(";
+
+*/
+
+function validParentheses(parens) {
+	return (/[()]+/ | /^\(\)$/).test(parens);
+}
+
+//   (/(^\(\)$)+/g
