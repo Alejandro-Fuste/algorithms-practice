3029d2def3452ba84fdcc93934df60e310bfea1b
Alejandro F <afuste7@gmail.com>
2020-10-13 13:03:46 -0400

    added for loop to capitalize the first character in the words that come after the first word

--- strings/stringToCamelCase.js
+++ strings/stringToCamelCase.js
@@ -22,16 +22,31 @@ Sample Tests:
 
 function toCamelCase(str) {
 	//   valid input to ensure it's not an empty string
+	if (typeof str != 'string' || str.length === 0 || /^\s*$/.test(str)) return 'Invalid Input';
+	// create array variable to push splits
+	let toBeJoined = [];
+
 	//   split the string
+	const delimiter = /[-_]/g;
+	let splitParts = str.split(delimiter);
+
 	//   push the first character into new array
-	//   find the delimiters
+	toBeJoined.push(splitParts[0]);
+
 	//   capitalize the first character after each delimiter
+	for (i = 1; i < toBeJoined.length; i++) {
+		// split the words that come after the first word
+		let splitWord = toBeJoined[i].split('');
+
+		//
+	}
+
 	//   push into new array
 	//   join array
 	//   return array
 }
 
-console.log(toCamelCase(''));
+// console.log(toCamelCase(''));
 console.log(toCamelCase('the_stealth_warrior'));
 console.log(toCamelCase('The-Stealth-Warrior'));
-console.log(toCamelCase('A-B-C'));
+// console.log(toCamelCase('A-B-C'));
