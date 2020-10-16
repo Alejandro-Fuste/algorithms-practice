72b6744cf2299a19428f38e0938980c7bd6dabdd
Alejandro F <afuste7@gmail.com>
2020-10-16 12:07:44 -0400

    changed code to trim out white spaces and check for length. Also, removed old return statement andchanged comments to reflect changes. Added console.logs to each function test

--- validation/validateString.js
+++ validation/validateString.js
@@ -10,15 +10,15 @@
 
 // Create function that will take in a string as an argument
 const validateString = (string) => {
-	// Use ternary operator that will return 'invalid string' if any of the three conditions is true
+	// Use ternary operator that will return 'invalid string' if any of the two conditions are true
 	// Conditions to check if data type is a string, if the string is empty, and if string contains only white spaces
 
-	return typeof string != 'string' || string.length === 0 || /^\s*$/.test(string) ? 'Invalid String' : 'Valid String';
+	return typeof string != 'string' || !string.trim().length ? 'Invalid String' : 'Valid String';
 };
 
-validateString('');
-validateString(8);
-validateString(null);
-validateString(undefined);
-validateString('  ');
-validateString('not empty');
+console.log(validateString(''));
+console.log(validateString(8));
+console.log(validateString(null));
+console.log(validateString(undefined));
+console.log(validateString('  '));
+console.log(validateString('not empty'));
