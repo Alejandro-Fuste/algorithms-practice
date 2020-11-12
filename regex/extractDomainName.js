5bebff3eb58aeb73f4dd0614ea3bd3f40a893697
Alejandro F <afuste7@gmail.com>
2020-11-12 16:13:55 -0500

    changed regex in replace method

--- regex/extractDomainName.js
+++ regex/extractDomainName.js
@@ -27,7 +27,7 @@ function domainName(url) {
 	// use empty string as second argument of replace method
 	// Return domain name
 
-	return url.replace(/(http:\/\/|https:\/\/|www\.|\.com|\.co|.jp|.ru)/g, '');
+	return url.replace(/(http:\/\/|https:\/\/|www\.)/g, '');
 }
 
 console.log(domainName('http://google.com'));
