4d683e7a8a9968eb8efa8f78192b0715185cb28a
Alejandro F <afuste7@gmail.com>
2020-11-17 14:31:23 -0500

    added function calls inside of console.logs

--- arrays/observedPin.js
+++ arrays/observedPin.js
@@ -49,6 +49,10 @@ function getPINs(observed) {
 	// TODO: This is your job, detective!
 }
 
+console.log(getPINs('8'));
+console.log(getPINs('11'));
+console.log(getPINs('369'));
+
 // Sample Tests:
 
 /*
