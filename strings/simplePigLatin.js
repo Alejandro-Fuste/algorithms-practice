a0b3c4c5861a76dbf1140d655b6dfa4721ba8b6f
Alejandro F <afuste7@gmail.com>
2020-12-04 12:14:10 -0500

    added split array variable and return keyword to function

--- strings/simplePigLatin.js
+++ strings/simplePigLatin.js
@@ -7,12 +7,14 @@
 
 function pigIt(str) {
 	// Split the str input to create an array of words & store in variable
+	let splitArray = str.split('');
 	// Split each word to get an array of letters
 	// Use shift to extract first letter
 	// Push first letter to the end
 	// Push ay to end
 	// Repeat previous process for each word
 	// Join all words into one string
+	return splitArray;
 }
 
 // Sample Tests
