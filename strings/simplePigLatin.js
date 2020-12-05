74eebe7a18929431ebf8c156ad6a57f15df70b74
Alejandro F <afuste7@gmail.com>
2020-12-04 20:27:45 -0500

    added code to function to split words in letters, then add first letter and 'ay' to the end of each word

--- strings/simplePigLatin.js
+++ strings/simplePigLatin.js
@@ -8,16 +8,24 @@
 function pigIt(str) {
 	// Split the str input to create an array of words & store in variable
 	let splitArray = str.split(' ');
-	// Split each word to get an array of letters
-	// Use shift to extract first letter
-	// Push first letter to the end
-	// Push ay to end
-	// Repeat previous process for each word
-	// Join all words into one string
-	return splitArray;
+	let splitWord = [];
+	let finalPhrase = [];
+
+	// With forEach loop, split each word to get an array of letters
+	splitArray.forEach((element) => {
+		splitWord.push(element.split(''));
+	});
+
+	// With another forEach loop, use shift to extract first letter, push first letter & 'ay' to the end
+	splitWord.forEach((c, i, a) => {
+		a[i].push(c.shift(), 'ay');
+	});
+
+	// Join all words into one string in return statement
+	return splitWord;
 }
 
 // Sample Tests
 console.log(pigIt('Pig latin is cool')); // igPay atinlay siay oolcay
-console.log(pigIt('Hello world !')); // elloHay orldway !
-console.log(pigIt('This is my string')); // 'hisTay siay ymay tringsay')
+// console.log(pigIt('Hello world !')); // elloHay orldway !
+// console.log(pigIt('This is my string')); // 'hisTay siay ymay tringsay')
