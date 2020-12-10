2f302a1d9c2fbe1208fdf2cbf67e4edf06edb3f7
Alejandro F <afuste7@gmail.com>
2020-12-10 13:49:42 -0500

    added better solution to file

--- strings/simplePigLatin.js
+++ strings/simplePigLatin.js
@@ -5,31 +5,36 @@
     marks untouched. 
 */
 
+// function pigIt(str) {
+// 	// Split the str input to create an array of words & store in variable
+// 	let splitArray = str.split(' ');
+// 	let splitWord = [];
+// 	let finalPhrase = [];
+
+// 	// With forEach loop, split each word to get an array of letters
+// 	splitArray.forEach((element) => {
+// 		splitWord.push(element.split(''));
+// 	});
+
+// 	// With another forEach loop, use shift to extract first letter, push first letter & 'ay' to the end
+// 	splitWord.forEach((c, i, a) => {
+// 		if (c.length <= 1 && /\W/g.test(c)) {
+// 			a[i].push(c);
+// 		} else {
+// 			a[i].push(c.shift(), 'ay');
+// 		}
+// 	});
+
+// 	// With another forEach loop, push each word into finalPhrase variable
+// 	splitWord.forEach((c, i, a) => finalPhrase.push(c.join('')));
+
+// 	// Join all words into one string in return statement
+// 	return finalPhrase.join(' ');
+// }
+
+// Better Solution
 function pigIt(str) {
-	// Split the str input to create an array of words & store in variable
-	let splitArray = str.split(' ');
-	let splitWord = [];
-	let finalPhrase = [];
-
-	// With forEach loop, split each word to get an array of letters
-	splitArray.forEach((element) => {
-		splitWord.push(element.split(''));
-	});
-
-	// With another forEach loop, use shift to extract first letter, push first letter & 'ay' to the end
-	splitWord.forEach((c, i, a) => {
-		if (c.length <= 1 && /\W/g.test(c)) {
-			a[i].push(c);
-		} else {
-			a[i].push(c.shift(), 'ay');
-		}
-	});
-
-	// With another forEach loop, push each word into finalPhrase variable
-	splitWord.forEach((c, i, a) => finalPhrase.push(c.join('')));
-
-	// Join all words into one string in return statement
-	return finalPhrase.join(' ');
+	return str.replace(/(\w)(\w*)(\s|$)/g, '$2$1ay$3');
 }
 
 // Sample Tests
