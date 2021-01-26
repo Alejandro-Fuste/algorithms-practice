64092c8e9d72d70bd50984e642df9911347f1744
Alejandro F <afuste7@gmail.com>
2021-01-26 14:25:15 -0500

    added variable to hold array of numbers to be sum

--- typeConversion/romanNumeralsHelper.js
+++ typeConversion/romanNumeralsHelper.js
@@ -36,6 +36,9 @@ class RomanNumerals {
 	// add fromRoman static method to class that takes in romanNumeral
 	static fromRoman(romanNumeral) {
 		const romanNumerals = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+
+		// variable to hold array of numbers that will need to be sum
+		let sumNumbers = [];
 	}
 }
 
