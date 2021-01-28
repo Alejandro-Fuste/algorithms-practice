d56684c7d759f4c46cff4d178ca0d3466885919a
Alejandro F <afuste7@gmail.com>
2021-01-28 14:26:40 -0500

    added numberObj variable to toRoman method

--- typeConversion/romanNumeralsHelper.js
+++ typeConversion/romanNumeralsHelper.js
@@ -30,8 +30,14 @@ class RomanNumerals {
 
 	// add toRoman static method to class that takes in number
 	static toRoman(number) {
-		const numbers = { 1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M' };
-		const multipliers = { 0: 1, 1: 1, 2: 10, 3: 100, 4: 1000 };
+		// const numbers = { 1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M' };
+		const numberObj = {
+			0: 0,
+			1: { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX' },
+			2: { 1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'LX', 6: 'LX', 7: 'LXX', 8: 'LXXX', 9: 'XC' },
+			3: { 1: 'C', 2: 'CC', 3: 'CCC', 4: 'CD', 5: 'D', 6: 'DC', 7: 'DCC', 8: 'DCCC', 9: 'CM' },
+			4: { 1: 'M', 2: 'MM', 3: 'MMM', 4: 'MMMM' }
+		};
 
 		// convert number to string & then split it
 		let stringifyNumber = number.toString().split('');
