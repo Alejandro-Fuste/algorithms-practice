2f48d471fd672eb8e3ccf9724a7a59229bf3781c
Alejandro F <afuste7@gmail.com>
2021-03-02 10:00:24 -0500

    added suite variable to create new Benchmark suite

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -1,9 +1,12 @@
 // Bring in benchmark package and generate file
 const [ Benchmark, generate ] = [ require('benchmark'), require('../generate') ];
 
-// Generate an array of random numbers of the given
+// Generate an array of random numbers of the given length
 let length = 10000;
 let stuff = generate(length);
 let randomValue = stuff[Math.ceil(Math.random() * length)];
 
-console.log(stuff, randomValue);
+// A "suite" is a series of code snippets you want
+//    to execute and time
+
+let suite = new Benchmark.Suite();
