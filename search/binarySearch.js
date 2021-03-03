2383c4de43b985c2a12760f695f4b512a34d03a5
Alejandro F <afuste7@gmail.com>
2021-03-03 10:47:11 -0500

    added array and randomValue paramenters to function and initial state variables for right, left, and middle.

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -1,3 +1,7 @@
-const binarySearch = () => {};
+const binarySearch = (array, randomValue) => {
+	let left = 0;
+	let right = array.length - 1;
+	let middle = Math.floor((right + left) / 2);
+};
 
 module.exports = binarySearch;
