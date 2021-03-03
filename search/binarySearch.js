a08db3a8e0a7319b94a201cb11f4342d4a96c15c
Alejandro F <afuste7@gmail.com>
2021-03-03 11:16:27 -0500

    removed left and right variables from functions and passed them in as arguments

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -1,6 +1,6 @@
-const binarySearch = (array, randomValue) => {
-	let left = 0;
-	let right = array.length - 1;
+const data = [ 12, 23, 38, 40, 54, 62, 71, 87, 99 ];
+
+const binarySearch = (array, randomValue, left, right) => {
 	let middle = Math.floor((right + left) / 2);
 
 	if (randomValue === array[middle]) {
@@ -8,7 +8,12 @@ const binarySearch = (array, randomValue) => {
 	} else if (randomValue < array[middle]) {
 		right = middle;
 		console.log(`Bring right down to ${right}`);
+	} else if (randomValue > array[middle]) {
+		left = middle + 1;
+		console.log(`Bring left up to ${left}`);
 	}
 };
+
+console.log(binarySearch(data, 54));
 
 module.exports = binarySearch;
