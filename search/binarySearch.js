d95bf1eb0de1a878ce5a2984fe4f7d227f63fcf8
Alejandro F <afuste7@gmail.com>
2021-03-03 10:56:05 -0500

    added if and else if statements to binarySearch function

--- search/binarySearch.js
+++ search/binarySearch.js
@@ -2,6 +2,13 @@ const binarySearch = (array, randomValue) => {
 	let left = 0;
 	let right = array.length - 1;
 	let middle = Math.floor((right + left) / 2);
+
+	if (randomValue === array[middle]) {
+		console.log(`Found your number (${randomValue})`);
+	} else if (randomValue < array[middle]) {
+		right = middle;
+		console.log(`Bring right down to ${right}`);
+	}
 };
 
 module.exports = binarySearch;
