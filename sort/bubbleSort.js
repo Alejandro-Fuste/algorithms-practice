a8986de62be333240ebe4f11ac4c0c308076dc3c
Alejandro F <afuste7@gmail.com>
2021-03-04 18:46:46 -0500

    added function call inside console.log

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -6,7 +6,10 @@ const bubbleSort = (array) => {
 
 		for (i = 0; i < array.length - 1; i++) {
 			if (array[i] > array[i + 1]) {
-				// then swap
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+
 				sorted = false;
 			}
 		}
@@ -14,4 +17,6 @@ const bubbleSort = (array) => {
 	return array;
 };
 
-module.exports = bubbleSort;
+// module.exports = bubbleSort;
+
+console.log(bubbleSort([ 54, 8, 87, 88, 100, 98, 17, 72, 67, 42, 40, 34, 55, 18, 33, 94, 59, 68, 92, 2 ]));
