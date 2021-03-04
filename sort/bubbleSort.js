9fbf1141da77f6b165aeee1bb58070f3d78e894b
Alejandro F <afuste7@gmail.com>
2021-03-04 18:09:42 -0500

    added nested for loop and return statement to function

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,7 +1,12 @@
 const bubbleSort = (array) => {
 	let sorted = false;
 
-	while (!sorted) {}
+	while (!sorted) {
+		sorted = true;
+
+		for (i = 0; i < array.length - 1; i++) {}
+	}
+	return array;
 };
 
 module.exports = bubbleSort;
