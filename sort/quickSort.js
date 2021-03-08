e98823fb86d42278176e832f55ae5e398a402156
Alejandro F <afuste7@gmail.com>
2021-03-08 11:44:47 -0500

    initialized quickSort function and move initial variables inside of function

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,11 +1,17 @@
 // Choose first item in array as pivot
 // Create variables for left and right arrays
 
-const [ pivot, left, right ] = [ array[0], [], [] ];
-
 // Loop through the array
+
+const quickSort = (array) => {
+	const [ pivot, left, right ] = [ array[0], [], [] ];
+
+	console.log(pivot, left, right);
+};
 // Push item in left variable if it's less than the pivot
 // Push item in right variable if it's greater than the pivot
 
 // Use recursion to repeat this process
 // Join left, pivot, right in return statement
+
+quickSort([ 0, 1 ]);
