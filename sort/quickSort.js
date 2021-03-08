8ade37b950394d949e8cc2615a233de814199af6
Alejandro F <afuste7@gmail.com>
2021-03-08 11:37:18 -0500

    added pseudocode to file

---
+++ sort/quickSort.js
@@ -0,0 +1,9 @@
+// Choose first item in array as pivot
+// Create variables for left and right arrays
+
+// Loop through the array
+// Push item in left variable if it's less than the pivot
+// Push item in right variable if it's greater than the pivot
+
+// Use recursion to repeat this process
+// Join left, pivot, right in return statement
