4ecbab7a18297cc30444fc86c8d43b2c06de6647
Alejandro F <afuste7@gmail.com>
2021-03-12 18:10:43 -0500

    added code to vowel-count file

--- strings/vowel-count/Unsolved/vowel-count.js
+++ strings/vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,9 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+	let count = 0;
+	str.toLowerCase().split('').forEach((c) => {
+		if ([ 'a', 'e', 'i', 'o', 'u' ].indexOf(c) != -1) count++;
+	});
+	return count;
+};
