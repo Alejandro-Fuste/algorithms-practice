51112894e9926b6a1ede6ec19486aedd0b04af51
Alejandro F <afuste7@gmail.com>
2021-03-12 17:10:28 -0500

    added code to max-num file

--- math/max-num/Unsolved/max-num.js
+++ math/max-num/Unsolved/max-num.js
@@ -1,3 +1,7 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+	const sorted = arr.sort((a, b) => a - b);
+
+	return sorted[sorted.length - 1];
+};
