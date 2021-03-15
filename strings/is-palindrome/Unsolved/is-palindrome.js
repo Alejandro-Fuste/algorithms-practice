5bed09480d8aa610adee03b4f0296ad64356f3cd
Alejandro F <afuste7@gmail.com>
2021-03-15 17:57:25 -0400

    added while loop solution in is-palindrom file

--- strings/is-palindrome/Unsolved/is-palindrome.js
+++ strings/is-palindrome/Unsolved/is-palindrome.js
@@ -10,11 +10,37 @@
 // };
 
 // refractored
-var isPalindrome = function(str) {
-	// removed variables and moved code to return statement
+// var isPalindrome = function(str) {
+// 	// removed variables and moved code to return statement
 
-	// Return the comparison of the str and reversed string
-	return str === str.split('').reverse().join('');
-};
+// 	// Return the comparison of the str and reversed string
+// 	return str === str.split('').reverse().join('');
+// };
 
 // different approach
+var isPalindrome = function(str) {
+	// instead of using reverse method, use loop
+	let left = 0;
+	let right = str.length - 1;
+
+	while (left <= right) {
+		if (str[left].charCodeAt(0) < 65 && str[right].charCodeAt(0) < 65) {
+			left++;
+			right--;
+		}
+		if (str[left].charCodeAt(0) < 65) {
+			left++;
+			right = right;
+		}
+		if (str[right].charCodeAt(0) < 65) {
+			right--;
+			left = left;
+		}
+		if (str[left].toLowerCase() != str[right].toLowerCase()) return false;
+		if (str[left].toLowerCase() == str[right].toLowerCase()) {
+			left++;
+			right--;
+		}
+	}
+	return true;
+};
