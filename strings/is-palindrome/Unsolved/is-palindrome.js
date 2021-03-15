72d41190332d413fbd040f7f1489e4baad75b402
Alejandro F <afuste7@gmail.com>
2021-03-15 14:33:29 -0400

    added refractored solution in is-palindrom file

--- strings/is-palindrome/Unsolved/is-palindrome.js
+++ strings/is-palindrome/Unsolved/is-palindrome.js
@@ -1,9 +1,20 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
+// var isPalindrome = function(str) {
+// 	// Need variables to store original string and reversed string
+// 	let original = str;
+// 	// Need to split, reverse, and join str
+// 	let reversed = str.split('').reverse().join('');
+// 	// Return the comparison of both variables
+// 	return original === reversed;
+// };
+
+// refractored
 var isPalindrome = function(str) {
-	// Need variables to store original string and reversed string
-	let original = str;
-	// Need to split, reverse, and join str
-	let reversed = str.split('').reverse().join('');
-	// Return the comparison of both variables
+	// removed variables and moved code to return statement
+
+	// Return the comparison of the str and reversed string
+	return str === str.split('').reverse().join('');
 };
+
+// different approach
