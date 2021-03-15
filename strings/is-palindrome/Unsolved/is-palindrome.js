d3ea9914b646ab3b0da0bd08ae4c78dd831e6b81
Alejandro F <afuste7@gmail.com>
2021-03-15 14:18:32 -0400

    added new is-palindrom  algorithm to string folder

---
+++ strings/is-palindrome/Unsolved/is-palindrome.js
@@ -0,0 +1,7 @@
+// Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
+
+var isPalindrome = function(str) {
+	// Need variables to store original string and reversed string
+	// Need to split, reverse, and join str
+	// Return the comparison of both variables
+};
