b4e58049927a3969f42914413c449c6a32ce3ecf
Alejandro F <afuste7@gmail.com>
2021-03-16 16:07:30 -0400

    added for loop to function

--- math/factorial/Unsolved/factorial.js
+++ math/factorial/Unsolved/factorial.js
@@ -4,5 +4,8 @@ var factorial = function(num) {
 	// guard clause to return 1 if num = 0
 	if (num === 0) return 1;
 	// Use for loop to pass in num as the conditional
+	for (i = 1; i <= num; i++) {
+		console.log(i);
+	}
 	// multiple numbers inside loop
 };
