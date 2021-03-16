6cdb605205c764c876d42e2de50a8e701920ebfa
Alejandro F <afuste7@gmail.com>
2021-03-16 18:05:56 -0400

    added code to correctly return factorial

--- math/factorial/Unsolved/factorial.js
+++ math/factorial/Unsolved/factorial.js
@@ -1,17 +1,17 @@
 // Write code to create a function that returns the factorial of `num`
 
-let factorial = function(num) {
-	// create total state variable
-	let total = 1;
-	// guard clause to return 1 if num = 0
-	if (num === 0) return 1;
-	// Use for loop to pass in num as the conditional
-	for (i = 1; i <= num; i++) {
-		// multiple numbers inside loop
-		total *= i;
-	}
-	return total;
-};
+// let factorial = function(num) {
+// 	// create total state variable
+// 	let total = 1;
+// 	// guard clause to return 1 if num = 0
+// 	if (num === 0) return 1;
+// 	// Use for loop to pass in num as the conditional
+// 	for (i = 1; i <= num; i++) {
+// 		// multiple numbers inside loop
+// 		total *= i;
+// 	}
+// 	return total;
+// };
 
 // refractored solution
 // use right and left variables to perform multiplication
@@ -20,7 +20,16 @@ let factorial = function(num) {
 let factorial = function(num) {
 	let [ left, right, middle ] = [ 1, num, Math.floor(num / 2) ];
 
-	for (i = 1; i < middle; i++) {}
+	for (i = 1; i < middle; i++) {
+		left *= i + 1;
+		right *= num - i;
+		console.log(left, right);
+	}
+
+	if (num % 2 != 0) return (middle + 1) * left * right;
+	return left * right;
 };
 
-module.exports = factorial;
+console.log(factorial(7));
+
+// module.exports = factorial;
