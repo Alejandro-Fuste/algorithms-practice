404591a35d82ba1dc317b2467226cf1937bab9f0
Alejandro F <afuste7@gmail.com>
2021-03-16 16:10:37 -0400

    added total variable to function

--- math/factorial/Unsolved/factorial.js
+++ math/factorial/Unsolved/factorial.js
@@ -1,11 +1,13 @@
 // Write code to create a function that returns the factorial of `num`
 
 var factorial = function(num) {
+	// create total state variable
+	let total = 1;
 	// guard clause to return 1 if num = 0
 	if (num === 0) return 1;
 	// Use for loop to pass in num as the conditional
 	for (i = 1; i <= num; i++) {
+		// multiple numbers inside loop
 		console.log(i);
 	}
-	// multiple numbers inside loop
 };
