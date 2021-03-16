e0983eb310b17c07578ba3eafb5223810803fb1e
Alejandro F <afuste7@gmail.com>
2021-03-16 16:03:11 -0400

    added guard clause to function

--- math/factorial/Unsolved/factorial.js
+++ math/factorial/Unsolved/factorial.js
@@ -2,6 +2,7 @@
 
 var factorial = function(num) {
 	// guard clause to return 1 if num = 0
+	if (num === 0) return 1;
 	// Use for loop to pass in num as the conditional
 	// multiple numbers inside loop
 };
