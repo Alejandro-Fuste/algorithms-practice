28e3f0100e6ecbce84f054e607c30fa925ee6ac9
Alejandro F <afuste7@gmail.com>
2021-03-16 16:00:23 -0400

    added pseudocode to factorial function

--- math/factorial/Unsolved/factorial.js
+++ math/factorial/Unsolved/factorial.js
@@ -1,3 +1,7 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+	// guard clause to return 1 if num = 0
+	// Use for loop to pass in num as the conditional
+	// multiple numbers inside loop
+};
