0ee45e09a79b499ae2b01365c056b63be3379cec
Alejandro F <afuste7@gmail.com>
2021-03-23 08:13:22 -0400

    added forEach method to function

--- strings/title-case/Unsolved/title-case.js
+++ strings/title-case/Unsolved/title-case.js
@@ -1,7 +1,11 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
 var titleCase = function(str) {
+	str.split(' ');
+
 	return str;
 };
+
+console.log(titleCase('a lannister always pays his debts'));
 
 module.exports = titleCase;
