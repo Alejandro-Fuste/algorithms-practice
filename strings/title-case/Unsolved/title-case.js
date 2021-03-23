70353cea500c5c17755f1bc1272f9388d00b89d2
Alejandro F <afuste7@gmail.com>
2021-03-23 10:24:47 -0400

    added newStr variable to function

--- strings/title-case/Unsolved/title-case.js
+++ strings/title-case/Unsolved/title-case.js
@@ -1,7 +1,11 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
 var titleCase = function(str) {
-	str.split(' ');
+	let newStr = [];
+	str.split(' ').forEach((c) => {
+		console.log(c[0].toUpperCase());
+		console.log(c);
+	});
 
 	return str;
 };
