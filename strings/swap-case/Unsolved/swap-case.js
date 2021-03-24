63fbb4d082a47c797003a6ca5b689c743c3dc679
Alejandro F <afuste7@gmail.com>
2021-03-24 11:19:13 -0400

    added newStr to return statement

--- strings/swap-case/Unsolved/swap-case.js
+++ strings/swap-case/Unsolved/swap-case.js
@@ -1,8 +1,8 @@
 // Write code to create a function takes a string and returns the string with all of the letter cases swapped
 
 var swapCase = function(str) {
-	let newStr = [];
+	let newStr = str.toUpperCase().split(' ').map((c) => c.replace(/[a-z]/, c.toLowerCase()));
 
-	return str.toUpperCase().split(' ').map((c) => c.toLowerCase());
+	return newStr;
 };
 console.log(swapCase('Luke, I Am Your Father'));
