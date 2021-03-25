3215f6504d88ba5f18bd643b9bce16af21905900
Alejandro F <afuste7@gmail.com>
2021-03-25 12:39:53 -0400

    added toString method and return statement to sort method

--- strings/longest-word/Unsolved/longest-word.js
+++ strings/longest-word/Unsolved/longest-word.js
@@ -1,7 +1,9 @@
 // Write code to create a function that accepts a string (`str`) and returns the longest word in the string
 
 var longestWord = function(str) {
-	let sortedString = str.split(' ');
+	let sortedString = str.split(' ').sort((a, b) => {
+		return b.length.toString() - a.length.toString();
+	});
 
 	return sortedString[0];
 };
