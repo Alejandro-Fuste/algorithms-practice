762624ef59fc94291eb42454684ec338aa0e01f6
Alejandro F <afuste7@gmail.com>
2021-03-26 07:54:49 -0400

    added console log with function call

--- strings/acronym-builder/Unsolved/acronym-builder.js
+++ strings/acronym-builder/Unsolved/acronym-builder.js
@@ -1,3 +1,5 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
 var acronymBuilder = function(str) {};
+
+console.log(acronymBuilder('For your information'));
