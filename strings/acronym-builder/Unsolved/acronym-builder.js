6df5c879dbae4af03d9b2aed527e523c998cfddc
Alejandro F <afuste7@gmail.com>
2021-03-26 08:22:24 -0400

    added slice and to UpperCase methods to map. Also, added join method to return statement

--- strings/acronym-builder/Unsolved/acronym-builder.js
+++ strings/acronym-builder/Unsolved/acronym-builder.js
@@ -1,9 +1,9 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
 var acronymBuilder = function(str) {
-	let acronym = '';
+	let acronym = str.split(' ').map((c) => c.slice(0, 1).toUpperCase());
 
-	return acronym;
+	return acronym.join('');
 };
 
 console.log(acronymBuilder('For your information'));
