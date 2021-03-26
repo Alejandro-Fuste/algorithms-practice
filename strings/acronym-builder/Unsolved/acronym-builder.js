74892ff395ae9ff7faaf0b517eb228aafe3fcc40
Alejandro F <afuste7@gmail.com>
2021-03-26 07:58:57 -0400

    added acronym variable to function

--- strings/acronym-builder/Unsolved/acronym-builder.js
+++ strings/acronym-builder/Unsolved/acronym-builder.js
@@ -1,5 +1,7 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
-var acronymBuilder = function(str) {};
+var acronymBuilder = function(str) {
+	let acronym = '';
+};
 
 console.log(acronymBuilder('For your information'));
