2d0ddd43bc90e339d9ce14acbf0a9b443365604e
Alejandro F <afuste7@gmail.com>
2021-03-29 12:03:35 -0400

    added reverse helper function

--- strings/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
+++ strings/01-reverse-no-built-in/Unsolved/reverse-no-built-in.js
@@ -2,5 +2,17 @@
 // You may NOT use the built-in reverse method
 
 var reverse = function(str) {
-
+	// split string at spaces
+	// reverse order of words
+	// reverse order of letters
 };
+
+function reverseHelper(str) {
+	let reversed = '';
+
+	for (i = 1; i <= str.length; i++) {
+		reversed += str[str.length - i];
+	}
+
+	return reversed;
+}
