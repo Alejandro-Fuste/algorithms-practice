ec302705c398268700ab900bc4d11afc9e4adbf7
Alejandro F <afuste7@gmail.com>
2021-04-02 10:42:10 -0400

    added more pseudocode to function

--- math/04-is-numeric/Unsolved/is-numeric.js
+++ math/04-is-numeric/Unsolved/is-numeric.js
@@ -3,5 +3,7 @@
 var isNumeric = function(str) {
 	// setup object to hold array for digits and special characters
 	// split and loop through arrays
-	// use indexof
+	// use indexof to find numbers in array
+	// use indexof and lastIndexOf to make sure there's only one special characters
+	// return true if both conditions are false
 };
