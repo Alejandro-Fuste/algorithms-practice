8dc14cca29d57e0ee4c4a6edf01db8631b8303d5
Alejandro F <afuste7@gmail.com>
2021-04-02 10:46:38 -0400

    added object to hold numbers and special characters

--- math/04-is-numeric/Unsolved/is-numeric.js
+++ math/04-is-numeric/Unsolved/is-numeric.js
@@ -2,6 +2,10 @@
 
 var isNumeric = function(str) {
 	// setup object to hold array for digits and special characters
+	const characters = {
+		numbers: [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ],
+		specialCharacters: [ '.', '+', '-' ]
+	};
 	// split and loop through arrays
 	// use indexof to find numbers in array
 	// use indexof and lastIndexOf to make sure there's only one special characters
