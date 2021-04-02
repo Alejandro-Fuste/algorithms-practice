4d1d9d90c1e7e3b03c31f1cfe53e032b79a943ac
Alejandro F <afuste7@gmail.com>
2021-04-02 08:16:02 -0400

    added pseudocode to function

--- math/04-is-numeric/Unsolved/is-numeric.js
+++ math/04-is-numeric/Unsolved/is-numeric.js
@@ -1,3 +1,7 @@
 // Write code to create a function that accepts a string and determines if the string is a valid number. You may not use any regex, built-in methods type conversion to accomplish this.
 
-var isNumeric = function(str) {};
+var isNumeric = function(str) {
+	// setup object to hold array for digits and special characters
+	// split and loop through arrays
+	// use indexof
+};
