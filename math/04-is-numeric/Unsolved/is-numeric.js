3779413f5f581668e28d8fc05ddddfbc771060b9
Alejandro F <afuste7@gmail.com>
2021-04-02 12:04:57 -0400

    added forEach loop to function

--- math/04-is-numeric/Unsolved/is-numeric.js
+++ math/04-is-numeric/Unsolved/is-numeric.js
@@ -6,8 +6,8 @@ var isNumeric = function(str) {
 		numbers: [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ],
 		specialCharacters: [ '.', '+', '-' ]
 	};
-	// split and loop through arrays
-	str.split('');
+	// split and loop through
+	str.split('').forEach((c) => {});
 	// use indexof to find numbers in array
 	// use indexof and lastIndexOf to make sure there's only one special characters
 	// return true if both conditions are false
