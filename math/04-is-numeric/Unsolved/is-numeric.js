9fa8126ca6836184d6b0bd6767971482a8005890
Alejandro F <afuste7@gmail.com>
2021-04-05 12:15:08 -0400

    added code to body of hasNum condition

--- math/04-is-numeric/Unsolved/is-numeric.js
+++ math/04-is-numeric/Unsolved/is-numeric.js
@@ -1,5 +1,7 @@
 // Write code to create a function that accepts a string and determines if the string is a valid number. You may not use any regex, built-in methods type conversion to accomplish this.
 
+const { has } = require('benchmark');
+
 var isNumeric = function(str) {
 	// setup object to hold true values for numbers
 	const numbers = { 0: true, 1: true, 2: true, 3: true, 4: true, 5: true, 6: true, 7: true, 8: true, 9: true };
@@ -9,8 +11,12 @@ var isNumeric = function(str) {
 
 	for (let i = 0; i < str.length; i++) {
 		let char = str[i];
-		if (str[0]) {
+
+		if (numbers[char]) {
+			hasNum = true;
+			continue;
 		}
+
 		if (str[0] === '+' || str[0] === '-') continue;
 
 		if (str[i] === '.') {
