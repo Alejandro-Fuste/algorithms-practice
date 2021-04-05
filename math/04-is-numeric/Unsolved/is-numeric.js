b3cc3f8b9a28da33033c7a495b0f604575b5c7ff
Alejandro F <afuste7@gmail.com>
2021-04-05 10:47:38 -0400

    added guard clause for empty string

--- math/04-is-numeric/Unsolved/is-numeric.js
+++ math/04-is-numeric/Unsolved/is-numeric.js
@@ -3,7 +3,10 @@
 var isNumeric = function(str) {
 	// setup object to hold true values for numbers
 	const numbers = { 0: true, 1: true, 2: true, 3: true, 4: true, 5: true, 6: true, 7: true, 8: true, 9: true };
+
+	if (!str.length) return false;
 };
 
 console.log(isNumeric('-0123.00')); // => true
 console.log(isNumeric('+-a13')); // => false
+console.log(isNumeric('')); // => false
