f54070764911975b29bfe62e88aa3bba9c10404d
Alejandro F <afuste7@gmail.com>
2021-10-28 13:39:16 -0400

    added comments to code

---
+++ databases/heap/maxHeap.js
@@ -0,0 +1,5 @@
+/* ------- Max Heap ------- */
+
+// Heap: a type of tree-like data structure that uses an array to store data
+
+// Max Heap:  is where the parent is always greater than any of its children
