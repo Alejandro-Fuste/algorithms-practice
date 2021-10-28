ab356bf442252188ff79baaff07538b9cdbd268e
Alejandro F <afuste7@gmail.com>
2021-10-28 19:43:17 -0400

    added comment to code

--- databases/heap/maxHeap.js
+++ databases/heap/maxHeap.js
@@ -7,10 +7,12 @@
 // right child: i * 2 + 1
 
 let MaxHeap = function () {
-  let heap = [null, 1, 2, 3];
+  let heap = [null];
 
   // print method
   this.print = () => console.log(heap);
+
+  // insert method
 };
 
 let test = new MaxHeap();
