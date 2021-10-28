7202533c3cd0b32d67e906a52ed6e976f1ba943e
Alejandro F <afuste7@gmail.com>
2021-10-28 19:41:13 -0400

    added MaxHeap function and print method

--- databases/heap/maxHeap.js
+++ databases/heap/maxHeap.js
@@ -1,1 +1,17 @@
 /* ------- Max Heap ------- */
+
+/* Node Equations */
+
+// parent: Math.floor(i/2)
+// left child: i * 2
+// right child: i * 2 + 1
+
+let MaxHeap = function () {
+  let heap = [null, 1, 2, 3];
+
+  // print method
+  this.print = () => console.log(heap);
+};
+
+let test = new MaxHeap();
+test.print();
